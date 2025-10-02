02493ccb33f70c24416f915e512a4d012fcba35c
Daria Eliseeva <elisey422das@gmail.com>
2025-10-02 14:00:35 +0200

    add inputs

--- pokemon-app/src/GenerationPart.jsx
+++ pokemon-app/src/GenerationPart.jsx
@@ -4,15 +4,44 @@ function GenerationPart() {
     <div className="generation-part item left">
       <img />
       <p> The type of the pokemon</p>
-      <div>
+      <section>
         <p> Name </p>
-        <input type="text" />
-      </div>
-      <div className="number-interaction">
-        <p> Number </p>
-        <button></button>
-        <input type="number" />
-        <button></button>
+        <input
+          type="text"
+          className="with-icon"
+          placeholder="Enter the name..."
+        />
+        <img
+          className="input-icon"
+          src="../images/glitter.svg"
+          alt="logo"
+          height="20px"
+          width="20px"
+        />
+      </section>
+      <div>
+        <p> National Pokedex Number</p>
+        <div className="number-interaction">
+          <button className="pokemon-number-button">
+            {" "}
+            <img
+              src="../images/arrow-down.svg"
+              alt="logo"
+              height="10px"
+              width="10px"
+            />
+          </button>
+          <input type="number" className="with-icon" />
+          <button className="pokemon-number-button">
+            {" "}
+            <img
+              src="../images/arrow-up.svg"
+              alt="logo"
+              height="10px"
+              width="10px"
+            />
+          </button>
+        </div>
       </div>
       <button className="search-button"> Search </button>
     </div>
--- pokemon-app/src/PokemonPalettePart.jsx
+++ pokemon-app/src/PokemonPalettePart.jsx
@@ -1,21 +1,24 @@
 import "./PokemonPalettePart.css";
 function PokemonPalettePart() {
   return (
-    <div className="pokemon-palette-part item right">
-      <div className="text-section">
-        <h2>A website inspired by colors.</h2>
-        <p>
-          Every Pokémon has its own unique palette. Enter a name or Pokédex
-          number, and this site will reveal the top three colors that bring your
-          Pokémon to life. Discover patterns, compare favorites, or just enjoy
-          exploring the colorful side of the Pokédex.
-        </p>
-        <div className="button-section">
-          <button className="scroll-button"> scroll down</button>
-          <input type="checkbox" className="theme" />
+    <div className="container item right">
+      <div className="pokemon-palette-part">
+        <div className="text-section">
+          <h2>A website inspired by colors.</h2>
+          <p>
+            Every Pokémon has its own unique palette. Enter a name or Pokédex
+            number, and this site will reveal the top three colors that bring
+            your Pokémon to life. Discover patterns, compare favorites, or just
+            enjoy exploring the colorful side of the Pokédex.
+          </p>
+          <div className="button-section">
+            <button className="scroll-button"> scroll down</button>
+            {/* <input type="checkbox" className="theme" /> */}
+          </div>
         </div>
+        <div className="image-section"></div>
       </div>
-      <div className="image-section"></div>
+      <div className="colors-section"></div>
     </div>
   );
 }
