e7e10ef83bef2ca98131c61f6c8c68edef30a722
Daria Eliseeva <elisey422das@gmail.com>
2025-10-02 10:04:50 +0200

    feat: restructure app layout and add generation and palette components

---
+++ pokemon-app/src/GenerationPart.jsx
@@ -0,0 +1,11 @@
+import "./GenerationPart.css";
+function GenerationPart() {
+  return (
+    <div className="generation-part item left">
+      <div className="first-color-block">h</div>
+      <div className="second-color-block">u</div>
+      <div className="third-color-block">i</div>
+    </div>
+  );
+}
+export default GenerationPart;
---
+++ pokemon-app/src/PokemonPalettePart.jsx
@@ -0,0 +1,14 @@
+import "./PokemonPalettePart.css";
+function PokemonPalettePart() {
+  return (
+    <div className="pokemon-palette-part item right">
+      <div>Pokemon Palette Part</div>
+      <h3> Your website inspired by colors</h3>
+      <p>
+        This website allows you to enter a Pokemon's name or simply it's number
+        in the Pokedex and its top 3 colors will be extracted
+      </p>
+    </div>
+  );
+}
+export default PokemonPalettePart;
